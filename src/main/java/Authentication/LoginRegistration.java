d236dbcc0cf96b8aa4cedc2bd11eaf5d1d3678b4
BaibolAsanbekov <asanbekovbaibol@gmail.com>
2024-05-17 17:13:56 +0600

    второй комит детка

--- src/main/java/Authentication/LoginRegistration.java
+++ src/main/java/Authentication/LoginRegistration.java
@@ -1,9 +1,11 @@
 package Authentication;
 
+import java.sql.*;
 import java.util.Scanner;
 
 public class LoginRegistration {
     private final Scanner scanner;
+
 
     public LoginRegistration(Scanner scanner) {
         this.scanner = scanner;
@@ -32,6 +34,37 @@ public class LoginRegistration {
         }
     }
 
+    private boolean registerToDatabase(String username, String password) {
+        try {
+            // Установка соединения с базой данных
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+
+            // Проверка существования пользователя с таким же именем
+            PreparedStatement checkStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            checkStatement.setString(1, username);
+            ResultSet resultSet = checkStatement.executeQuery();
+            if (resultSet.next()) {
+                System.out.println("Ошибка: Пользователь с таким именем уже существует.");
+                return false;
+            }
+
+            // Регистрация нового пользователя
+            PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
+            insertStatement.setString(1, username);
+            insertStatement.setString(2, password);
+            insertStatement.executeUpdate();
+
+            // Закрытие соединения с базой данных
+            connection.close();
+
+            return true; // Регистрация прошла успешно
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Ошибка при регистрации
+        }
+    }
+
+
     public void authenticateUser() {
         System.out.println("====== Аутентификация ======");
         System.out.print("Введите имя пользователя: ");
@@ -40,11 +73,53 @@ public class LoginRegistration {
         String password = scanner.nextLine();
 
         // Проверка аутентификации пользователя в базе данных
-        boolean authenticationSuccess = authenticateInDatabase(username, password);
+        boolean authenticationSuccess = authInDatabase(username, password);
         if (authenticationSuccess) {
             System.out.println("Аутентификация прошла успешно.");
         } else {
             System.out.println("Ошибка: Неправильное имя пользователя или пароль.");
         }
     }
+
+    private boolean authInDatabase(String username, String password) {
+        try {
+            // Установка соединения с базой данных
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+
+            // Поиск пользователя с заданным именем и паролем
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+
+            // Проверка наличия пользователя в базе данных
+            boolean userExists = resultSet.next();
+
+            // Закрытие соединения с базой данных
+            connection.close();
+
+            return userExists; // Возвращает true, если пользователь существует и аутентификация прошла успешно
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Ошибка при аутентификации
+        }
+    }
+
+    public void authenticateAdmin() {
+        System.out.println("====== Аутентификация Админа ======");
+        System.out.print("Введите имя пользователя: ");
+        String username = scanner.nextLine();
+        System.out.print("Введите пароль: ");
+        String password = scanner.nextLine();
+
+        // Проверка аутентификации пользователя в базе данных
+        boolean authenticationSuccess = authInDatabase(username, password);
+        if (authenticationSuccess) {
+            System.out.println("Аутентификация прошла успешно.");
+        } else {
+            System.out.println("Ошибка: Неправильное имя пользователя или пароль.");
+        }
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
-import java.sql.*;
+import Authentication.LoginRegistration;
 import java.util.Scanner;
 
+
 public class Main {
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        LoginRegistration LR = new LoginRegistration(scanner);
+
         while (true) {
             System.out.println("Добро пожаловать в салон оптики!");
             System.out.println("Выберите роль:");
@@ -18,13 +24,16 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    //adminMenu();
+                    LR.authenticateAdmin(); // вход админа
+                    adminMenu();
                     break;
-                case 2://Рега клиента
-                    //adminMenu();
+                case 2:
+                    LR.registerUser(); // регистер клиента
+                    LR.authenticateUser();
                     break;
                 case 3:
-                    //clientMenu(); Вход как клиент
+                    LR.authenticateUser(); // вход клиента
+                    clientMenu();
                     break;
                 case 4:
                     System.out.println("До свидания!");
@@ -50,17 +59,44 @@ public class Main {
         int choice = scanner.nextInt();
         scanner.nextLine(); // очистка буфера
 
-        // Здесь можно добавить вызов соответствующих методов для обработки выбора администратора
+        switch (choice) {
+            case 1:
+                // Просмотр заказов
+                break;
+            case 2:
+                // Создать заказ
+                break;
+            case 3:
+                // Удалить заказ
+                break;
+            case 4:
+                // Изменить статус заказа
+                break;
+            case 5:
+                // Просмотр всех товаров
+                break;
+            case 6:
+                //  Добавить новый товар
+                break;
+            case 7:
+                // Удалить товар
+                break;
+            case 8:
+                System.out.println("До свидания!");
+                return;
+            default:
+                System.out.println("Пожалуйста, выберите правильный вариант.");
+        }
+
+
     }
     public static void clientMenu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Меню клиента:");
-        System.out.println("1. Зарегистрироваться");
-        System.out.println("2. Войти");
-        System.out.println("3. Сделать заказ");
-        System.out.println("4. Просмотреть свои заказы");
-        System.out.println("5. Посмотреть каталог товаров");
-        System.out.println("6. Вернуться в главное меню");
+        System.out.println("1. Сделать заказ");
+        System.out.println("2. Просмотреть свои заказы");
+        System.out.println("3. Посмотреть каталог товаров");
+        System.out.println("4. Вернуться в главное меню");
 
         int choice = scanner.nextInt();
         scanner.nextLine(); // очистка буфера
